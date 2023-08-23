d3ce46c7f9d358eaf701cb8d6a389416e07dc7b8
unknown <janoncarlos@gmail.com>
2023-08-23 15:56:27 -0300

    database

--- index.js
+++ index.js
@@ -1,23 +1,28 @@
 const express = require("express");
 require("dotenv").config();
+const cors = require("cors");
+const { dbConnection } = require("./database/config");
 
-//Crear el servidor de express
+// Crear el servidor de express
 const app = express();
 
-//Lectura y parseo del body
+// Base de datos
+dbConnection();
+
+// CORS
+app.use(cors());
+
+// Directorio Público
+app.use(express.static("public"));
+
+// Lectura y parseo del body
 app.use(express.json());
 
 // Rutas
-
-//Todo lo que este archivo va a exportar lo va a habilitar
-//en esta ruta
 app.use("/api/auth", require("./routes/auth"));
-// TODO CRUD: Eventos
+// TODO: CRUD: Eventos
 
-//Directorio publico
-app.use(express.static("public"));
-
-//Escuchar peticiones
+// Escuchar peticiones
 app.listen(process.env.PORT, () => {
   console.log(`Servidor corriendo en puerto ${process.env.PORT}`);
 });
