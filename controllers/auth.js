bf69187acf3d4629926555318bf6a0dfe8f8ff8a
unknown <janoncarlos@gmail.com>
2023-08-23 17:54:03 -0300

    database user

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,5 @@
 const { response } = require("express");
+const bcrypt = require("bcryptjs");
 const { validationResult } = require("express-validator");
 const User = require("../models/User");
 
@@ -14,9 +15,13 @@ const createUser = async (req, res = response) => {
       });
     }
     user = new User(req.body);
+    // encript password
+    const salt = bcrypt.genSaltSync();
+    user.password = bcrypt.hashSync(password, salt);
     await user.save();
-    res.status(201).json({ ok: true, msg: "registro" });
+    res.status(201).json({ ok: true, uid: user.id, name: user.name });
   } catch (error) {
+    console.log(error);
     res.status(500).json({
       ok: false,
       msg: "Contact Admin",
@@ -24,10 +29,36 @@ const createUser = async (req, res = response) => {
   }
 };
 
-const loginUser = (req, res = response) => {
+const loginUser = async (req, res = response) => {
   const { email, password } = req.body;
 
-  res.json({ ok: true, msg: "registro", email, password });
+  try {
+    let user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(400).json({
+        ok: false,
+        msg: "User not valid",
+      });
+    }
+    //confirmar contrasenas
+    const validPassword = bcrypt.compareSync(password, user.password);
+    if (!validPassword) {
+      return res.status(400).json({
+        ok: false,
+        msg: "Password not valid",
+      });
+    }
+    res.json({ ok: true, uid: user.id, name: user.name });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: "Contact Admin",
+    });
+  }
+
+  // res.json({ ok: true, msg: "registro", email, password });
 };
 
 const renewToken = (req, res = response) => {
