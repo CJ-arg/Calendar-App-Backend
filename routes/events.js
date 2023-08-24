2b4c1f5d5056ac21ad2cfbb89a5aa95649b8fac6
unknown <janoncarlos@gmail.com>
2023-08-24 11:29:34 -0300

    routes events

--- routes/events.js
+++ routes/events.js
@@ -1,4 +1,43 @@
+const { Router } = require("express");
+const { check } = require("express-validator");
+const { isDate } = require("../helpers/isDate");
+const { validarCampos } = require("../middlewares/validar-campos");
+const { validarJWT } = require("../middlewares/validar-jwt");
+const {
+  getEventos,
+  crearEvento,
+  actualizarEvento,
+  eliminarEvento,
+} = require("../controllers/events");
+const router = Router();
 
+router.use(validarJWT);
 
+router.get("/", getEventos);
+// post un nuevo evento
+router.post(
+  "/",
+  [
+    check("title", "El titulo es obligatorio").not().isEmpty(),
+    check("start", "Fecha de inicio es obligatoria").custom(isDate),
+    check("end", "Fecha de finalización es obligatoria").custom(isDate),
+    validarCampos,
+  ],
+  crearEvento
+);
+// update Evento
+router.put(
+  "/:id",
+  [
+    check("title", "El titulo es obligatorio").not().isEmpty(),
+    check("start", "Fecha de inicio es obligatoria").custom(isDate),
+    check("end", "Fecha de finalización es obligatoria").custom(isDate),
+    validarCampos,
+  ],
+  actualizarEvento
+);
 
+// delete evento
+router.delete("/:id", eliminarEvento);
 
+module.exports = router;
