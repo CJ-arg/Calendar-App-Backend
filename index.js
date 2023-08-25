4a13b935114e646c7cedca196f321d7e7441f295
unknown <janoncarlos@gmail.com>
2023-08-25 10:42:07 -0300

    add events DB

--- index.js
+++ index.js
@@ -5,23 +5,17 @@ const { dbConnection } = require("./database/config");
 
 // Crear el servidor de express
 const app = express();
-
 // Base de datos
 dbConnection();
-
 // CORS
 app.use(cors());
-
 // Directorio Público
 app.use(express.static("public"));
-
 // Lectura y parseo del body
 app.use(express.json());
-
 // Rutas
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/events", require("./routes/events"));
-// TODO: CRUD: Eventos
 
 // Escuchar peticiones
 app.listen(process.env.PORT, () => {
